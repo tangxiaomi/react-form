cffabd61f27148f6d15d95674d72a5a25861a973
tangxiaomi <1759125339@qq.com>
2025-07-08 15:30:03 +0800

    add extra libraries for project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 新增导入
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/variables" as *;`,
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 配置别名
+    }
+  }
 })
